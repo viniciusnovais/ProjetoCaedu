3f75ca42aca1d8adf96b9e5a651b0c2b2a573d81
Vinicius da Mata <v.novais@outlook.com>
2017-09-26 18:30:52 -0300

    criação do modelo produto

---
+++ app/src/main/java/pdasolucoes/com/br/projetocaedu/mobile/Model/Produto.java
@@ -0,0 +1,20 @@
+package pdasolucoes.com.br.projetocaedu.mobile.Model;
+
+/**
+ * Created by PDA on 26/09/2017.
+ */
+
+public class Produto {
+
+    private String codProduto;
+    private String descricao;
+    private float preco;
+    private String cor;
+    private String tamanho;
+    private String idade;
+    private String tipo_produto;
+    private int prop;
+    private String margem;
+
+
+}
